86158a2fc19603bd49c2d85207e8adfcc3e62384
Kokona <985025074syc@gmail.com>
2026-05-20 16:57:19 +0800

    chore: add formatting config

--- src/trap/mod.rs
+++ src/trap/mod.rs
@@ -13,6 +13,4 @@ pub use crate::arch::loongarch64::trap::{
     get_current_token, init_trap, trap_from_kernel, trap_handler, trap_return,
 };
 #[cfg(target_arch = "riscv64")]
-pub use crate::arch::riscv64::trap::{
-    get_current_token, init_trap, trap_handler, trap_return,
-};
+pub use crate::arch::riscv64::trap::{get_current_token, init_trap, trap_handler, trap_return};
